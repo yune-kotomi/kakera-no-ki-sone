7786e02708f16144abcc1f0be21979a75e4756c8
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2015-03-05 14:42:43 +0900

    アーカイブへのリンク設置

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-  def link_to_user(label = nil, user = @login_user)
+  def link_to_user(label = nil, user = @login_user, options = {})
     label = user.nickname if label.nil?
-    link_to label,
+    link_to label, options.update(
       :controller => :users,
       :action => :show,
       :domain_name => user.domain_name,
       :screen_name => user.screen_name
+    )
   end
 end
