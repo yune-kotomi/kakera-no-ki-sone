49e0fd2914f18c9cdb25b8530fe1bd936ad08eb1
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2015-05-03 16:50:57 +0900

    attributes追加

--- app/assets/javascripts/editor/models/node.js.rb
+++ app/assets/javascripts/editor/models/node.js.rb
@@ -1,13 +1,28 @@
 module Editor
   module Model
     class Node < Juso::Model::Base
+      attribute :id
       attribute :title
       attribute :body
-      attribute :metadata, :default => {}
+      attribute :metadatum, :default => {}
+      attribute :children, :default => [], :type => Node
+
+      def initialize(data = {}, parent = nil)
+        super(data)
+        @parent = parent
+      end
     end
 
     class Root < Node
+      attribute :private, :default => true
+      attribute :archived, :default => false
+      attribute :password
+      attribute :markup, :default => 'plaintext'
+
       def save
+        HTTP.post("/documents/#{self.id}.json", :payload => self.attributes.to_json) do |response|
+          yield(response)
+        end
       end
     end
   end
