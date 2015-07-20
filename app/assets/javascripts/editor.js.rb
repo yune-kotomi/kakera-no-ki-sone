0b2e9cf4ca2cbfe8a7db0afd025ff262df910b2f
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2015-07-20 12:21:20 +0900

    DOMからの文書読み込み処理を追加 Squashed commit of the following:
    commit 45706cc0eda0c51066e87950c571877d1309293c
    Author: Yune Kotomi <yune-kotomi@users.noreply.github.com>
    Date:   Sat Jul 18 22:47:46 2015 +0900
    
        WIP
    
    commit 6f23419c925f3896151696edd9fb1634ab06ed4f
    Author: Yune Kotomi <yune-kotomi@users.noreply.github.com>
    Date:   Sat Jul 18 22:46:23 2015 +0900
    
        WIP
    
    commit d6c0cd05f9feb89f7e267866e4382bfd31c578e8
    Author: Yune Kotomi <yune-kotomi@users.noreply.github.com>
    Date:   Sun Jul 12 13:33:06 2015 +0900
    
        WIP

--- app/assets/javascripts/editor.js.rb
+++ app/assets/javascripts/editor.js.rb
@@ -1,2 +1,38 @@
 module Editor
+  class Editor
+    attr_reader :document
+
+    def load_from_dom
+      id = Element.find('#document-id').value
+      title = Element.find('#document-title').value
+      description = Element.find('#document-description').value
+      children = JSON.parse(Element.find('#document-body').value)
+      private = JSON.parse(Element.find('#document-private').value)
+      archived = JSON.parse(Element.find('#document-archived').value)
+      markup = JSON.parse(Element.find('#document-markup').value)
+
+      @document = Editor::Model::Root.new(
+        :id => id,
+        :title => title,
+        :body => body,
+        :children => children,
+        :private => private,
+        :archived => archived,
+        :markup => markup
+      )
+    end
+
+    def attach(element)
+      @tree = Editor::View::Tree.new(@document.tree)
+      @contents = Editor::View::Contents.new(@document.contents)
+    end
+  end
+end
+
+Document.ready? do
+  if Element.find('#document-editor')
+    editor = Editor::Editor.new
+    editor.load_from_dom
+    editor.attach(Element.find('#document-editor'))
+  end
 end
