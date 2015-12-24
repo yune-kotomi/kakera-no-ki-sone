1f7505c71c7f9a2cbca6f325981d10c0983c5163
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2015-12-24 19:46:17 +0900

    travisでpg_hint_planを有効に

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+ActiveRecord::Base.connection.execute("LOAD 'pg_hint_plan';")
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
