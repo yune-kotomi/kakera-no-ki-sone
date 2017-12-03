0cc65eb422753938f254e928a101a589a61a3954
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2017-12-03 21:18:07 +0900

    選択状態の変更では保存フラグを立てないように

--- app/assets/javascripts/editor2/writer/hosted.js.rb
+++ app/assets/javascripts/editor2/writer/hosted.js.rb
@@ -3,7 +3,7 @@ module Editor2
     def initialize(editor, interval = 5)
       @editor = editor
       @save = false
-      @sent_data = editor.store.stored_document
+      @sent_data = current_document
       @timer =
         Timer::Timer.new(interval) do
           transmit if @save
@@ -12,21 +12,16 @@ module Editor2
     end
 
     def write
-      @save = true
-      @editor.indicate_save(:on)
+      unless current_document == @sent_data
+        @save = true
+        @editor.indicate_save(:on)
+      end
     end
 
     private
     # PATCH /documents/ID.jsonを発行する
     def transmit
-      data = @editor.store.stored_document
-      data = {
-        :title => data[:title],
-        :description => data[:body],
-        :body => data[:children].to_json,
-        :public => (data[:published] == true),
-        :markup => data[:markup]
-      }
+      data = current_document
 
       if data == @sent_data
         # 内容が更新されていなければ保存しない
@@ -43,6 +38,17 @@ module Editor2
           end
         end
       end
+    end
+
+    def current_document
+      doc = @editor.store.stored_document
+      {
+        :title => doc[:title],
+        :description => doc[:body],
+        :body => doc[:children].to_json,
+        :public => (doc[:published] == true),
+        :markup => doc[:markup]
+      }
     end
   end
 end
